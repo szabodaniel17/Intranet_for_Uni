245e2c3a973770432a4b0de55a1a755446f57a3d
Urcan Denisa <denisa_1272@yahoo.com>
2021-01-04 16:00:11 +0200

    professor flow

--- js/login.js
+++ js/login.js
@@ -1,3 +1,21 @@
 $(document).ready(function (){
-    
+
+    $('#submit').on("click", verifyCredentials);
 })
+
+function verifyCredentials(){
+
+    let username = $('#username');
+    let password = $('#password');
+
+    if(username.val() == 'denisaurcan@student.utcluj.ro' && password.val() == 'pass1234'){
+        // dashboard student
+    }else if(username.val() == 'cheresioana@professor.utcluj.ro' && password.val() == 'pass1234'){
+        //dashboard profesor
+        window.location.href = "index_prof.html";
+    }else if(username.val() == 'cheresioana@worker.utcluj.ro' && password.val() == 'pass1234'){
+        //dashboard personal administrativ
+    }
+    
+    return false;
+}

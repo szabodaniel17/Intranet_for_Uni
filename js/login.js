6a7ec3c08fc27bddfb88674d3ee58800a256b7fc
SzaboDaniel17 <daniel.szabo.schuller@gmail.com>
2021-01-06 21:51:59 +0200

    evaluare form + email valid

--- js/login.js
+++ js/login.js
@@ -1,3 +1,39 @@
 $(document).ready(function (){
+<<<<<<< Updated upstream
     
 })
+=======
+
+    $('#submit').on("click", verifyCredentials);
+})
+
+function verifyCredentials(){
+
+    let username = $('#username');
+    let password = $('#password');
+
+    if(!validateEmail(username.val())){
+        alert("Username not valid");
+    }
+
+    console.log(validateEmail(username.val()));
+
+    if(username.val() == 'toaderclaudiu@student.utcluj.ro' && password.val() == 'pass1234'){
+        // dashboard student
+        window.location.href = "index_student.html"
+    }else if(username.val() == 'cheresioana@professor.utcluj.ro' && password.val() == 'pass1234'){
+        //dashboard profesor
+        window.location.href = "index_prof.html";
+    }else if(username.val() == 'szabodaniel@worker.utcluj.ro' && password.val() == 'pass1234'){
+        //dashboard personal administrativ
+        window.location.href = "index_administrativ.html";
+    }
+    
+    return false;
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  }
+>>>>>>> Stashed changes

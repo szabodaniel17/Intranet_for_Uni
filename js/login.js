8962ecc17063293c1c65ce055497767399699d16
Urcan Denisa <denisa_1272@yahoo.com>
2021-01-11 14:08:37 +0200

    courses added for professor

--- js/login.js
+++ js/login.js
@@ -18,6 +18,14 @@ function verifyCredentials(){
     }else if(username.val() == 'szabodaniel@worker.utcluj.ro' && password.val() == 'pass1234'){
         //dashboard personal administrativ
         window.location.href = "index_administrativ.html";
+    }else {
+        swal({
+            title: "Incorrect username or password",
+            type: "warning",
+            showCancelButton: false,
+            confirmButtonText: "Try again!",
+            closeOnConfirm: false
+            });
     }
     
     return false;
